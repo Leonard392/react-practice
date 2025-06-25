4f6a6b9f825eabbfe610982dc0bfa9944fc38120
Leonardo Rodriguez <106454639+Leonard392@users.noreply.github.com>
2025-06-25 03:18:08 +0000

    trabajando estilos de cartas de brands

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import BrandCard from './components/BrandCard.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -20,14 +21,14 @@ function App() {
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        </button>        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div>
+        <BrandCard/>
+        <BrandCard/>
+        <BrandCard/>
+      </div>
     </>
   )
 }
---
+++ frontend/src/components/BrandCard.jsx
@@ -0,0 +1,10 @@
+import '../styles/BrandCard.css';
+
+ export default function BrandCard(){
+    return (
+        <div>
+          <h1>marca</h1>
+          <p>descripcion de la marca</p>
+        </div> 
+    )
+ }
